62c3bd2eb56eb190e147e0124d827715750ff759
Nilofar <nilofarsheriff@gmail.com>
2022-11-01 23:27:43 +0530

    Add project files.

---
+++ FirstBank_App/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstBank_App.Models;
+
+namespace FirstBank_App.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Verify(Account A)
+        {
+            if (A.Name == "Nilo" && A.Password == "123")
+            {
+                ViewBag.Name=A.Name;
+                return View("Verify");
+
+            }
+            else
+            {
+                return View("ErrorAc");
+            }
+            
+
+        }
+    }
+}
---
+++ FirstBank_App/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstBank_App.Models
+{
+    public class Account
+    {
+        public string? Name { get; set; }
+        public string? Account_Number { get; set; }
+        public string? Password { get; set; }    
+    }
+}
